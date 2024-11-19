3675a7fcafcd03df36e8f10f283100737235643a
David Starner <prosfilaes@gmail.com>
2024-11-19 13:12:29 -0600

    rotation & FPS

--- main.py
+++ main.py
@@ -15,10 +15,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill ((0, 0, 0))
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000 # convert from milliseconds to seconds
+        dt = clock.tick(FPS) / 1000 # convert from milliseconds to seconds
 
 if __name__ == "__main__":
     main()
